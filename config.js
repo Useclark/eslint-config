bebbf4945794cd1331cfdb5ff7bfe93959e0941b
Denis Sokolov <denis@sokolov.cc>
2015-05-27 10:16:32 +0300

    Documentation review, add more rules

--- config.js
+++ config.js
@@ -8,8 +8,10 @@ module.exports = {
     'block-scoped-var': 2,
     'default-case': 2,
     'guard-for-in': 2,
+    'strict': [2, 'global'],
     'no-plusplus': 2,
     'no-self-compare': 2,
+    'no-throw-literal': 2,
     'no-void': 2,
     'no-warning-comments': 2,
     'no-undefined': 2,
@@ -17,6 +19,13 @@ module.exports = {
 
     // Style
     'comma-style': [1, 'last'],
+    'dot-location': [1, 'property'],
+    'func-style': [1, 'expression'],
+    'linebreak-style': 1,
+    'no-continue': 1,
+    'no-inline-comments': 1,
+    'no-nested-ternary': 1,
+    'no-unneeded-ternary': 1,
     'quote-props': [1, 'as-needed'],
     'quotes': [1, 'single'],
 
